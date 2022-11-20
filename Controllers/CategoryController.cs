dd720e9231e8cb7a5d5b49f67a38a0db6835427d
Ange-Sepdeu <chriskameni25@gmail.com>
2022-11-20 17:27:43 +0100

    last

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace project2.Controllers
             if(ModelState.IsValid){
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category added successfully !";
             return RedirectToAction("Index");
             }
             return View(obj);
@@ -64,6 +65,7 @@ namespace project2.Controllers
             if(ModelState.IsValid){
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully !";
             return RedirectToAction("Index");
             }
             return View(obj);
@@ -91,6 +93,7 @@ namespace project2.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully !";
             return RedirectToAction("Index");
         }
 
